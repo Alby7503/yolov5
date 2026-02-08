c71082ffc09ab470c12c57559e082b66421c504d
Alberto Vona <alberto.vona24@gmail.com>
2026-02-08 17:19:17 +0100

    refactor: streamline model loading process in train function

--- train.py
+++ train.py
@@ -209,20 +209,21 @@ def train(hyp, opt, device, callbacks):
     is_coco = isinstance(val_path, str) and val_path.endswith("coco/val2017.txt")  # COCO dataset
 
     # Model
-    check_suffix(weights, ".pt")  # check weights
+    check_suffix(weights, ".pt")
     pretrained = weights.endswith(".pt")
     if pretrained:
         with torch_distributed_zero_first(LOCAL_RANK):
-            weights = attempt_download(weights)  # download if not found locally
-        ckpt = torch_load(weights, map_location="cpu")  # load checkpoint to CPU to avoid CUDA memory leak
-        model = Model(cfg or ckpt["model"].yaml, ch=3, nc=nc, anchors=hyp.get("anchors")).to(device)  # create
-        exclude = ["anchor"] if (cfg or hyp.get("anchors")) and not resume else []  # exclude keys
-        csd = ckpt["model"].float().state_dict()  # checkpoint state_dict as FP32
-        csd = intersect_dicts(csd, model.state_dict(), exclude=exclude)  # intersect
-        model.load_state_dict(csd, strict=False)  # load
-        LOGGER.info(f"Transferred {len(csd)}/{len(model.state_dict())} items from {weights}")  # report
+            weights = attempt_download(weights)
+        ckpt = torch_load(weights, map_location="cpu")
+        model = Model(cfg or ckpt["model"].yaml, ch=3, nc=nc, anchors=hyp.get("anchors")).to(device)
+        exclude = ["anchor"] if (cfg or hyp.get("anchors")) and not resume else []
+        csd = ckpt["model"].float().state_dict()
+        csd = intersect_dicts(csd, model.state_dict(), exclude=exclude)
+        model.load_state_dict(csd, strict=False)
+        LOGGER.info(f"Transferred {len(csd)}/{len(model.state_dict())} items from {weights}")
     else:
         model = Model(cfg, ch=3, nc=nc, anchors=hyp.get("anchors")).to(device)  # create
+    
     amp = check_amp(model)  # check AMP
 
     # Freeze
